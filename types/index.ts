45625a5b74c8e996b5b83f9ac39fe595b65a431a
Horror26 <1032210438@tcetmumbai.in>
2024-04-06 19:17:41 +0530

    types

---
+++ types/index.ts
@@ -0,0 +1,10 @@
+export type CreateUserParams = {
+  username: string;
+  dob: Date;
+  gender: string;
+  photo: string;
+  career: string;
+  married: string;
+  relationship: string;
+  position: string;
+};
